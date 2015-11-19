2721c21d3cf6422bf3525f6ac3deaa96d8e025dd
jespe <jespe@10.10.141.75>
2015-11-19 12:09:49 +0100

    Edited getName

---
+++ KASgit/src/application/model/Participant.java
@@ -0,0 +1,13 @@
+package application.model;
+
+public class Participant {
+private String name;
+
+public String getName() {
+	return name + "hej";
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+}
